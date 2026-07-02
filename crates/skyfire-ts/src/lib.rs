da416eed0113436078ec6859bbe984073076eede
Alex Fishlock <alex.fishlock@racingjag.com>
2026-07-03 00:17:58 +0100

    style: rustfmt import order after mpeg_ts::resync repoint

--- crates/skyfire-ts/src/lib.rs
+++ crates/skyfire-ts/src/lib.rs
@@ -13,11 +13,11 @@ use broadcast_common::traits::Parse;
 use dvb_pes::{PesAssembler, PesPacket};
 use dvb_si::demux::SiDemux;
 use dvb_si::descriptors::any::AnyDescriptor;
-use mpeg_ts::resync::TsResync;
 use dvb_si::tables::any::AnyTableSection;
 use dvb_si::tables::pat::PatSection;
 use dvb_si::tables::pmt::StreamType;
 use dvb_subtitle::{AnySegment, PesDataField};
+use mpeg_ts::resync::TsResync;
 
 use std::collections::HashMap;
 
